200da8f02e62a6349e643e5fc54c5001bad68fa4
parthibanktech <parthiban.k.tech3@gmail.com>
2025-12-13 12:14:05 +0700

    Fix docker-compose network configuration

--- init-databases.sql
+++ init-databases.sql
@@ -1,14 +1,23 @@
--- Create databases for each microservice
-CREATE DATABASE student_db;
-CREATE DATABASE course_db;
-CREATE DATABASE enrollment_db;
+-- Initialize Databases for Microservices
+-- This script runs automatically on the first container startup
+
+-- 1. Payment Service
 CREATE DATABASE payment_db;
+
+-- 2. Library Service
 CREATE DATABASE library_db;
+
+-- 3. Infrastructure Service
 CREATE DATABASE infrastructure_db;
 
--- Grant privileges (optional, depending on your setup)
--- GRANT ALL PRIVILEGES ON DATABASE student_db TO postgres;
--- GRANT ALL PRIVILEGES ON DATABASE course_db TO postgres;
--- GRANT ALL PRIVILEGES ON DATABASE enrollment_db TO postgres;
--- GRANT ALL PRIVILEGES ON DATABASE payment_db TO postgres;
--- GRANT ALL PRIVILEGES ON DATABASE library_db TO postgres;
+-- 4. Course Service (Separated from student_db)
+CREATE DATABASE course_db;
+
+-- 5. Enrollment Service (Separated from student_db)
+CREATE DATABASE enrollment_db;
+
+-- 6. Student Service (Default DB)
+-- Note: 'student_db' is created automatically by the POSTGRES_DB environment variable in docker-compose.yml.
+-- However, we can include it here safely (it will fail silently if exists or be ignored).
+-- To be explicit:
+-- CREATE DATABASE student_db; 
